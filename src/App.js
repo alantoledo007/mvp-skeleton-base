d860efb690ea9176d3eefacc07fe5ed273cd3f9f
Alan Toledo <alantoledo.work@gmail.com>
2021-09-02 02:42:42 -0300

    testing react router dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
 export default function App() {
-  return <div>¡hola mundo!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <p>home</p>
+          <Link to="/test">test</Link>
+        </Route>
+        <Route exact path="/test">
+          <p>test</p>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
