f742b04deabeded1a1e8177f21a128be17a87676
Alan Toledo <alantoledo.work@gmail.com>
2021-09-01 22:36:37 -0300

    fist-commit

---
+++ config/webpack.common.js
@@ -0,0 +1,35 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+      },
+      {
+        type: "asset",
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
