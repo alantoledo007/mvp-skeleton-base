c79021a4f90a118e197980967d6e0da96c35100e
Alan Toledo <alantoledo.work@gmail.com>
2021-09-06 23:12:14 -0300

    feature/register

--- src/pages/RegisterPage/index.js
+++ src/pages/RegisterPage/index.js
@@ -1,10 +1,15 @@
 import RegisterForm from '@/components/forms/RegisterForm';
+import { registerWithEmailAndPassword } from '@/firebase/auth';
 
 export default function RegisterPage() {
+  const onSubmit = (data) => {
+    registerWithEmailAndPassword(data.email, data.password, data.fullname);
+  };
+
   return (
     <div>
       <h1>Register</h1>
-      <RegisterForm onSubmit={() => console.log('sssss')} />
+      <RegisterForm onSubmit={onSubmit} />
     </div>
   );
 }
