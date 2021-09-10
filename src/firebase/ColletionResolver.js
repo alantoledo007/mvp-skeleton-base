0e783d7604d31b2068039150941c8f0ba66eef44
Alan Toledo <alantoledo.work@gmail.com>
2021-09-09 23:02:28 -0300

    Update ColletionResolver.js

--- src/firebase/ColletionResolver.js
+++ src/firebase/ColletionResolver.js
@@ -3,3 +3,23 @@ import firebase from 'firebase/app';
 export default function CollectionResolver(collection) {
   this.collection = firebase.firestore().collection(collection);
 }
+
+CollectionResolver.prototype.get = function () {
+  return this.collection.get();
+};
+
+CollectionResolver.prototype.create = function (data) {
+  return this.collection.set(data);
+};
+
+CollectionResolver.prototype.update = function (doc, data) {
+  return this.collection.doc(doc).update(data);
+};
+
+CollectionResolver.prototype.updateTransaction = function (doc, cb) {
+  return this.collection.doc(doc).transaction(cb);
+};
+
+CollectionResolver.prototype.delete = function (doc) {
+  return this.collection.doc(doc).delete();
+};
